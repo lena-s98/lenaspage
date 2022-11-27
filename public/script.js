df44c89785604b3cc7232db96e4c34330233b02a
Lena <lena.sterz98@gmail.com>
2022-11-27 18:04:24 +0100

    add about me section text + bootstrap icon in skill section

--- public/script.js
+++ public/script.js
@@ -8,3 +8,10 @@ burgerMenu.addEventListener("click", () => {
 		buttons.classList.add("hidden");
 	}
 });
+
+const leftArrow = document.getElementById("chevron-left");
+const rightArrow = document.getElementById("chevron-right");
+
+leftArrow.addEventListener("click", () => {});
+
+rightArrow.addEventListener("click", () => {});
